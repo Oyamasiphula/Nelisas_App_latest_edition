47103316436b80fc00a2a6d1240911e559892594
oyama <oyama@projectcodex.co>
2015-09-16 13:30:58 +0200

    purchases table is now manipulated and fully updated

--- Data_Storage/purchases.sql
+++ Data_Storage/purchases.sql
@@ -1,7 +1,7 @@
-INSERT INTO Purchases_td(Purchases_date ,Product_id ,qTy ,Suppliers_id ,product_price)
-SELECT stock_purchases_csv.date,stock_purchases_csv.item,stock_purchases_csv.quantity, Products_td.id AS Product_name, Suppliers_td.id AS Suppliers_name
+INSERT INTO Purchases_td(Purchases_date,qTy,Product_id,Supplier_id,product_price)
+SELECT stock_purchases_csv.date,stock_purchases_csv.quantity, Products_td.id AS product_id, Suppliers_td.id AS supplier_id,stock_purchases_csv.cost
 FROM stock_purchases_csv 
 INNER JOIN Products_td
-ON Products_td.Product_name = stock_purchases_csv.stock_item;
+ON Products_td.Product_name = stock_purchases_csv.item
 INNER JOIN Suppliers_td
-ON Suppliers_td.id = stock_purchases_csv.shop;
+ON Suppliers_td.Supplier_name = stock_purchases_csv.shop;
